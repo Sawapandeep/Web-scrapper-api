878fe06d5835b2a203ffda1ae3d4031a5b4c1c0e
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2023-09-11 22:24:53 +0530

    Update server.js
    handling data cloning

--- server.js
+++ server.js
@@ -88,15 +88,113 @@
 // app.listen(PORT, () => {
 //   console.log(`Server started at ${PORT}`);
 // });
+// const express = require('express');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+// const PORT = process.env.PORT || 5000;
+// const app = express();
+// const base = "https://gamerant.com";
+// const cors =require ('cors');
+// const resources = { name: 'gamerant', address: 'https://gamerant.com/', base: 'https://gamerant.com/' };
+// const newsData = []; // Initialize an empty array to store news data
+
+// // Function to check if a news item is older than 6 hours
+// function isOlderThan6Hours(time) {
+//   const sixHoursAgo = new Date();
+//   sixHoursAgo.setHours(sixHoursAgo.getHours() - 6);
+//   return new Date(time) < sixHoursAgo;
+// }
+// app.use(cors({
+//   origin: '*',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// }));
+
+// app.get('/fetch', async (req, res) => {
+//   try {
+//     // Call the /news route and await the response
+//     const newsResponse = await axios.get(`${req.protocol}://${req.get('host')}/news`);
+//     const latestNewsData = newsResponse.data;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.status(200).json(newsData); // Return the newsData array as the response
+//   } catch (err) {
+//     res.status(500).json({ error: 'Error fetching the latest news data.' });
+//   }
+// });
+// app.get('/news', async (req, res) => {
+//   axios
+//     .get(resources.address)
+//     .then((response) => {
+//       const html = response.data;
+//       const $ = cheerio.load(html);
+
+      
+// $('div.sentinel-home-list div.display-card.article.small').each((index, element) => {
+//   const $element = $(element);
+//   const title = $element.find('h5.display-card-title a').text().trim();
+//   const titleLink = resources.base + $element.find('h5.display-card-title a').attr('href').trim();
+//   const description = $element.find('p.display-card-excerpt').text().trim();
+//   const datetime = $element.find('time.display-card-date').attr('datetime'); // Get the datetime attribute
+
+//   // Check if the news item is older than 6 hours
+//   if (!isOlderThan6Hours(datetime)) {
+//     const imageSources = [];
+//     $element.find('div.bc-img.responsive-img.img-displayCard > figure > picture > source:first-of-type').each((imgIndex, imgElement) => {
+//       const imgSource = $(imgElement).attr('srcset');
+//       if (imgSource) {
+//         // Extract the URL from srcset attribute
+//         const [url, size] = imgSource.split(' ').filter((value) => value.trim() !== '');
+//         imageSources.push(url);
+//       }
+//     });
+
+//     const brandTags = [];
+//     $element.find('a.tag-label-text.primary-tag.brand-tag').each((tagIndex, tagElement) => {
+//       const brandTag = $(tagElement).text().trim();
+//       brandTags.push(brandTag);
+//     });
+
+//     // Convert the datetime to a numeric timestamp (Unix timestamp)
+//     const timestamp = new Date(datetime).getTime();
+
+//     const newsItem = {
+//       title,
+//       titleLink,
+//       description,
+//       timestamp, // Use the numeric timestamp
+//       imageSources,
+//       brandTags,
+//     };
+
+//     newsData.push(newsItem); // Append to the end of the newsData array
+  
+
+//         }
+//       });
+
+//       // Sort the newsData array based on time (latest to oldest)
+//       newsData.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+//       console.log('Latest news data updated in newsData');
+//       res.json({ message: 'Latest news data updated successfully.', data: newsData });
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).json({ error: 'Error fetching data from the website.' });
+//     });
+// });
+
+
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const base = "https://gamerant.com";
-const cors =require ('cors');
+const cors = require('cors');
 const resources = { name: 'gamerant', address: 'https://gamerant.com/', base: 'https://gamerant.com/' };
-const newsData = []; // Initialize an empty array to store news data
+let newsData = []; // Initialize an empty array to store news data
 
 // Function to check if a news item is older than 6 hours
 function isOlderThan6Hours(time) {
@@ -104,6 +202,7 @@ function isOlderThan6Hours(time) {
   sixHoursAgo.setHours(sixHoursAgo.getHours() - 6);
   return new Date(time) < sixHoursAgo;
 }
+
 app.use(cors({
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -113,6 +212,9 @@ app.use(cors({
 
 app.get('/fetch', async (req, res) => {
   try {
+    // Clear the newsData array before fetching new data
+    newsData = [];
+
     // Call the /news route and await the response
     const newsResponse = await axios.get(`${req.protocol}://${req.get('host')}/news`);
     const latestNewsData = newsResponse.data;
@@ -122,6 +224,7 @@ app.get('/fetch', async (req, res) => {
     res.status(500).json({ error: 'Error fetching the latest news data.' });
   }
 });
+
 app.get('/news', async (req, res) => {
   axios
     .get(resources.address)
@@ -129,49 +232,7 @@ app.get('/news', async (req, res) => {
       const html = response.data;
       const $ = cheerio.load(html);
 
-      
-$('div.sentinel-home-list div.display-card.article.small').each((index, element) => {
-  const $element = $(element);
-  const title = $element.find('h5.display-card-title a').text().trim();
-  const titleLink = resources.base + $element.find('h5.display-card-title a').attr('href').trim();
-  const description = $element.find('p.display-card-excerpt').text().trim();
-  const datetime = $element.find('time.display-card-date').attr('datetime'); // Get the datetime attribute
-
-  // Check if the news item is older than 6 hours
-  if (!isOlderThan6Hours(datetime)) {
-    const imageSources = [];
-    $element.find('div.bc-img.responsive-img.img-displayCard > figure > picture > source:first-of-type').each((imgIndex, imgElement) => {
-      const imgSource = $(imgElement).attr('srcset');
-      if (imgSource) {
-        // Extract the URL from srcset attribute
-        const [url, size] = imgSource.split(' ').filter((value) => value.trim() !== '');
-        imageSources.push(url);
-      }
-    });
-
-    const brandTags = [];
-    $element.find('a.tag-label-text.primary-tag.brand-tag').each((tagIndex, tagElement) => {
-      const brandTag = $(tagElement).text().trim();
-      brandTags.push(brandTag);
-    });
-
-    // Convert the datetime to a numeric timestamp (Unix timestamp)
-    const timestamp = new Date(datetime).getTime();
-
-    const newsItem = {
-      title,
-      titleLink,
-      description,
-      timestamp, // Use the numeric timestamp
-      imageSources,
-      brandTags,
-    };
-
-    newsData.push(newsItem); // Append to the end of the newsData array
-  
-
-        }
-      });
+      // Rest of your news scraping code...
 
       // Sort the newsData array based on time (latest to oldest)
       newsData.sort((a, b) => new Date(b.time) - new Date(a.time));
@@ -185,7 +246,9 @@ $('div.sentinel-home-list div.display-card.article.small').each((index, element)
     });
 });
 
-
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
